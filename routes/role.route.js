c9a0aee5f5d522de7899acbed56c77b1edde9d0f
lamba <lam45883@gmail.com>
2025-02-22 04:50:42 +0700

    Set up real-time message

--- routes/role.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {createRole , getAllRoles, getRoleById } from "../controllers/roleController.js";
-
-const router = express.Router();
-
-router.route("/").get(getAllRoles).post(createRole)
-router.route("/:id").get(getRoleById)
-
-export default router
--- routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { login } from "../controllers/userController.js";
-import authMiddleware from "../middleware/auth.js";
-
-router.route("/login").post(login);
-
-export default router;
